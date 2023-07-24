e87ba418e662584b11185378860d5e8169645f7a
Taviiiaa <99610159+Taviiiaa@users.noreply.github.com>
2023-07-23 21:20:07 -0400

    Update mysql_test.py

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -1,3 +1,63 @@
+CREATE USER 'pysports_user'@'localhost'  IDENTIFIED WITH mysql_native_password BY  'Hazel0101!' ;
+
+GRANT ALL PRIVILEGES ON pysports.* TO 'pysports_user'@'localhost'
+
+-- drop test user if exists 
+DROP USER IF EXISTS 'pysports_user'@'localhost';
+
+CREATE TABLE team (
+    team_id     INT             NOT NULL        AUTO_INCREMENT,
+    team_name   VARCHAR(75)     NOT NULL,
+    mascot      VARCHAR(75)     NOT NULL,
+    PRIMARY KEY(team_id)
+); 
+
+CREATE TABLE player (
+    player_id   INT             NOT NULL        AUTO_INCREMENT,
+    first_name  VARCHAR(255)     NOT NULL,
+    last_name   VARCHAR(255)     NOT NULL,
+    team_id     INT             NOT NULL,
+    PRIMARY KEY(player_id),
+    CONSTRAINT fk_team 
+    FOREIGN KEY(team_id)
+        REFERENCES team(team_id)
+);
+
+
+#Insert 
+INSERT INTO team(team_name, mascot)
+    VALUES('Team Red ', 'Red Robins');
+
+INSERT INTO team(team_name, mascot)
+    VALUES('Team Blue', 'Blue Birds');
+
+-- drop tables if they are present
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS team;
+
+--Insert player into the teams
+
+INSERT INTO team(team_name, mascot) VALUES('Team Red', 'Red Robins'); INSERT INTO team(team_name, mascot) VALUES('Team Blue', 'Blue Birds');
+
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Michelle', 'Grace', (SELECT team_id FROM team WHERE team_name = 'Team Blue'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Larry', 'Haze', (SELECT team_id FROM team WHERE team_name = 'Team Blue'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Zach', 'Baldwin', (SELECT team_id FROM team WHERE team_name = 'Team Blue'));
+
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Bailey', 'Jones', (SELECT team_id FROM team WHERE team_name = 'Team Red'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Molly', 'Brown', (SELECT team_id FROM team WHERE team_name = 'Team Red'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Justin', 'Bieber', (SELECT team_id FROM team WHERE team_name = 'Team Red'));
+
+
 #Import_into_database
 
 import mysql.connector 
